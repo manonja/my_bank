09a0c59aeef321eea8f0b3e1d64ac386dc15d09a
Manon Jacquin <manon.jacquin@gmail.com>
2019-04-15 14:53:39 +0100

    show page done

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ApplicationRecord
   belongs_to :user
+
+  def amount_and_currency
+    self.amount.to_s + ' ' + self.currency_name
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :email, confirmation: true
 
-
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
 
 end
